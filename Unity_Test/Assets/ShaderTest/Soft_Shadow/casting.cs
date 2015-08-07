3ff1da9fa9a2d62990ef2a95d272669a377a3b46
sasuke010101 <sasuke010101@gmail.com>
2015-08-07 11:21:31 +0800

    sphere soft shadow receive and casting

---
+++ Unity_Test/Assets/ShaderTest/Soft_Shadow/casting.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class casting : MonoBehaviour {
+
+	public GameObject occluder;
+	
+	// Update is called once per frame
+	void Update () {
+		if(occluder != null)
+		{
+			this.renderer.sharedMaterial.SetVector("_SpherePosition", occluder.transform.position);
+
+			this.renderer.sharedMaterial.SetFloat("_ShpereRadius", occluder.transform.localScale.x / 2.0f);
+		}
+	}
+}
